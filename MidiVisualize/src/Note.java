01ae67369f19e5ad8d500b674973f8ab452202c1
jackgolden <jackgolden@192.168.1.199>
2019-07-31 18:15:02 -0400

    First commit

---
+++ MidiVisualize/src/Note.java
@@ -0,0 +1,32 @@
+
+
+public class Note {
+	Visualizer parent;
+
+	static final double PI = 3.14159265;
+	private int note, color;
+	float r, x, y; // represents position in z direction
+
+	long creationTime = System.nanoTime();
+
+	Note(Visualizer p, int Color, int note) { // constructor
+		parent = p;
+		this.color = Color;
+		this.note = note;
+
+		r = parent.getHeight() / 2 - 10;
+	}
+
+	void draw() { // display note with respect to time
+		parent.fill(color);
+		x = (float) (r * Math.sin(2 * PI * note / 12) + parent.getWidth() / 2);
+		y = (float) (r * -Math.cos(2 * PI * note / 12) + parent.getHeight() / 2);
+		parent.ellipse((float) (r * Math.sin(2 * PI * note / 12) + parent.getWidth() / 2),
+				(float) (r * -Math.cos(2 * PI * note / 12) + parent.getHeight() / 2), r / 10, r / 10);
+	}
+
+	void update(long currentTime) {
+		double elapsedTime = (currentTime - creationTime) / 100000000000.0;// //seconds since creation
+		r *= 1 / (elapsedTime + 1);
+	}
+}
---
+++ MidiVisualize/src/Visualizer.java
@@ -0,0 +1,76 @@
+import processing.core.PApplet;
+import sojamo.drop.*;
+import java.util.*;
+import java.io.*;
+
+public class Visualizer extends PApplet {
+	private static final long serialVersionUID = 1L;// need a list with time as connection point.
+
+	public static final String[] NOTE_NAMES = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+	public SDrop drop;
+
+	ArrayList<Note> Notes = new ArrayList<Note>();
+
+	final long creation_time = System.nanoTime();
+	long last_time = System.nanoTime();
+
+	public static void main(String[] args) {
+		PApplet.main("Visualizer");
+	}
+
+	public void setup() {
+		// size(800, 800);
+		size(displayWidth, displayHeight);
+		noStroke();
+		drop = new SDrop(this);
+	}
+
+	public boolean sketchFullScreen() {
+		return true;
+	}
+
+	public void draw() { // traverse the song
+		background(0);
+		// fill(0); // draw black circle
+		// ellipse(getWidth() / 2, getHeight() / 2, getHeight(), getHeight()); //draw
+		// black circle
+		fill(255);
+		for (int i = 0; i < Notes.size(); i++) {
+			Note n = Notes.get(i);
+			n.update(System.nanoTime());
+			n.draw();
+		}
+
+		fill(255);
+		for (int i = 0; i < 12; i++)
+			text(NOTE_NAMES[i], (float) ((getHeight() / 2 - 10) * Math.sin(2 * PI * i / 12) + getWidth() / 2),
+					(float) ((getHeight() / 2 - 10) * -Math.cos(2 * PI * i / 12) + getHeight() / 2));
+
+		// draw lines between active notes
+//		for(Note n : Notes)
+//			if(n.r > 10)
+//				for(Note x : Notes)
+//					if(x.r > 10)
+//						line(n.x, n.y, x.x, x.y);
+
+		if (Notes.size() > 70) {
+			Notes.remove(0);
+		}
+	}
+
+	public void noteTrigger(int key, int channel, Boolean state) {
+		int color = (int) map(key, 0, 127, 0, 255);
+		if (state) {
+			Note test = new Note(this, color, key);
+			Notes.add(test);
+		}
+	}
+
+	public void dropEvent(DropEvent theDropEvent) {
+		if (theDropEvent.isFile()) {
+			File myFile = theDropEvent.file();
+			Player MidiPlayer = new Player(myFile, this);
+			MidiPlayer.start();
+		}
+	}
+}
